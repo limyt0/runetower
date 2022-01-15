76d5d34f6579c424d8b361a4a1c90f0a18582c82
limyt0 <dam45@naver.com>
2022-01-15 22:24:38 +0900

    일부 commit

---
+++ Assets/3_3F/2_Script/basketTrigger.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using TMPro;
+using UnityEngine;
+
+public class basketTrigger : MonoBehaviour
+{
+    public GameObject lune3;
+    public Transform lune3Point;
+    int count = 0;
+    int one = 0;
+    float time = 0;
+    bool cube_true=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (count == 3 && one == 0 && cube_true == true)
+        {
+            time += Time.deltaTime;
+            if (time >= 3f)
+            {
+                Instantiate(lune3, lune3Point.position, lune3Point.rotation);
+                one = 1;
+            }
+        }
+    }
+
+    
+
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag=="item")
+        {
+            count++;
+            other.tag = "none";
+            
+        }
+        if(other.tag=="firewood_fire"&&count==3)
+        {
+            cube_true = true;
+        }
+    }
+}
+
+
